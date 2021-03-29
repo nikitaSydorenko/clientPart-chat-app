52f81a34bbd3f1f024383b4255e08da7a50a7af5
sidorenkonikitka@growthside.app <sidorenkonikitka@growthside.app>
2021-03-29 17:13:40 +0300

    added socket io and lyout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { io }from "socket.io-client";
 import './styles.css';
 
-const App = () => (
-  <div>
-    hello
-  </div>
-);
+let socket;
+const CONNECTION_PORT = 'http://localhost:3001/';
+const App = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [userName, setUserName] = useState('');
+    const [room, setRoom] = useState('');
+
+    useEffect(() => {
+        socket = io(CONNECTION_PORT);
+    },[CONNECTION_PORT]);
+
+    const handleChangeName = useCallback((event) => {
+        setUserName(event.target.value)
+    }, []);
+
+    const handleChangeRoom = useCallback((event) => {
+        setRoom(event.target.value)
+    },[]);
+
+    const connectToRoom = () => {
+        setLoggedIn(true)
+        socket.emit('join_room', room)
+    }
+    return (
+        <div className="App">
+            {!loggedIn ? (
+                <div className='logIn'>
+                    <div className="inputs">
+                        <input type="text" placeholder='Name' onChange={handleChangeName}/>
+                        <input type="text" placeholder='Room' onChange={handleChangeRoom}/>
+                    </div>
+                    <button onClick={connectToRoom}>Enter chat</button>
+                </div>
+            ) : <h1>You are logged in</h1> //later I make up the chat container
+            }
+        </div>
+    )
+}
 
 export default App;
