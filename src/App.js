73f864876e7c7ca27cdff8a5b7e11e5eb6b9151f
sidorenkonikitka@growthside.app <sidorenkonikitka@growthside.app>
2021-03-30 10:56:25 +0300

    added validation for textfields and use state for socket

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { io }from "socket.io-client";
 import './styles.css';
+import {CONNECTION_URL} from "./utils/api/ports";
 
-let socket;
-const CONNECTION_PORT = 'http://localhost:3001/';
 const App = () => {
-
     const [loggedIn, setLoggedIn] = useState(false);
     const [userName, setUserName] = useState('');
     const [room, setRoom] = useState('');
+    const [errors, setErrors] = useState(false);
+    const [socket, setSocket] = useState(null)
 
     useEffect(() => {
-        socket = io(CONNECTION_PORT);
-    },[CONNECTION_PORT]);
+        setSocket(io(CONNECTION_URL))
+    },[]);
 
     const handleChangeName = useCallback((event) => {
         setUserName(event.target.value)
@@ -22,10 +22,15 @@ const App = () => {
         setRoom(event.target.value)
     },[]);
 
-    const connectToRoom = () => {
-        setLoggedIn(true)
-        socket.emit('join_room', room)
-    }
+    const connectToRoom = useCallback(() => {
+        if(!room || !userName){
+            setErrors(true)
+        }else {
+            setLoggedIn(true)
+            socket.emit('join_room', room)
+        }
+    },[socket, errors]);
+
     return (
         <div className="App">
             {!loggedIn ? (
@@ -34,7 +39,8 @@ const App = () => {
                         <input type="text" placeholder='Name' onChange={handleChangeName}/>
                         <input type="text" placeholder='Room' onChange={handleChangeRoom}/>
                     </div>
-                    <button onClick={connectToRoom}>Enter chat</button>
+                    {errors ? <span className='err'>Name or Room cannot be empty!</span> : null}
+                    <button onClick={connectToRoom} >Enter chat</button>
                 </div>
             ) : <h1>You are logged in</h1> //later I make up the chat container
             }
@@ -43,3 +49,4 @@ const App = () => {
 }
 
 export default App;
+
